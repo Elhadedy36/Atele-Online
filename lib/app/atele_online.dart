e611090120853f190cf20da9922d2bce298d83a9
omar elhadedy <elhadede36@gmail.com>
2024-11-27 20:53:45 +0200

    Responsive screen using screen utils

--- lib/app/atele_online.dart
+++ lib/app/atele_online.dart
@@ -1,14 +1,21 @@
 import 'package:atele_online/core/routes/app_router.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class AteleOnline extends StatelessWidget {
   const AteleOnline({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp.router(
-      debugShowCheckedModeBanner: false,
-      routerConfig: router,
-    );
+    return ScreenUtilInit(
+        designSize: Size(360, 690),
+        minTextAdapt: true,
+        splitScreenMode: true,
+        builder: (context, child) {
+          return MaterialApp.router(
+            debugShowCheckedModeBanner: false,
+            routerConfig: router,
+          );
+        });
   }
 }
