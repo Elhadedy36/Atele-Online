e3282b6b9eba6cbe403f6d04bd90b31d401940ed
omar elhadedy <elhadede36@gmail.com>
2025-01-09 22:30:27 +0200

    fix adding appointment id to all appoint collections and subcollections

--- lib/feature/reservations/presentation/cubit/reserve_cubit.dart
+++ lib/feature/reservations/presentation/cubit/reserve_cubit.dart
@@ -17,57 +17,64 @@ class ReserveCubit extends Cubit<ReserveState> {
       FirebaseFirestore.instance.collection(FirebaseStrings.appointments);
   final CollectionReference _firestoreProduct =
       FirebaseFirestore.instance.collection(FirebaseStrings.products);
-  final CollectionReference _firestoresub = FirebaseFirestore.instance
+  final CollectionReference _firestoreSubUser = FirebaseFirestore.instance
       .collection(FirebaseStrings.users)
       .doc(FirebaseAuth.instance.currentUser!.uid)
       .collection(FirebaseStrings.appointments);
+  final CollectionReference _firestoreSubSeller =
+      FirebaseFirestore.instance.collection(FirebaseStrings.sellers);
 
   Future<void> addAppointment(
       ProductModel product, String date, String time) async {
     try {
+      Map<String, dynamic> appointmentsData = {
+        FirebaseStrings.productName: product.productName,
+        FirebaseStrings.categoryName: product.categoryName,
+        FirebaseStrings.ateleName: product.ateleName,
+        FirebaseStrings.phoneNumber: product.phoneNumber,
+        FirebaseStrings.address: product.address,
+        FirebaseStrings.appointmentDate: date,
+        FirebaseStrings.appointmentTime: time,
+        FirebaseStrings.depositeAmount: product.depositeAmount,
+        FirebaseStrings.price: product.price,
+        FirebaseStrings.rest: product.price - product.depositeAmount,
+        FirebaseStrings.productsImages: product.productImages,
+        FirebaseStrings.isForRent: product.isForRent,
+        FirebaseStrings.productId: product.productId,
+        FirebaseStrings.userId: FirebaseAuth.instance.currentUser!.uid,
+        FirebaseStrings.status: FirebaseStrings.statusPending,
+        FirebaseStrings.sellerId: product.sellerId,
+        FirebaseStrings.appointmentId: '',
+      };
       // First, create the document in the global appointments collection
-      DocumentReference docRef = await _firestoreappointment.add({
-        FirebaseStrings.productName: product.productName,
-        FirebaseStrings.categoryName: product.categoryName,
-        FirebaseStrings.ateleName: product.ateleName,
-        FirebaseStrings.phoneNumber: product.phoneNumber,
-        FirebaseStrings.address: product.address,
-        FirebaseStrings.appointmentDate: date,
-        FirebaseStrings.appointmentTime: time,
-        FirebaseStrings.depositeAmount: product.depositeAmount,
-        FirebaseStrings.price: product.price,
-        FirebaseStrings.rest: product.price - product.depositeAmount,
-        FirebaseStrings.productsImages: product.productImages,
-        FirebaseStrings.isForRent: product.isForRent,
-        FirebaseStrings.productId: product.productId,
-        FirebaseStrings.userId: FirebaseAuth.instance.currentUser!.uid,
-        FirebaseStrings.status: FirebaseStrings.statusPending,
-      });
+      DocumentReference docRef1 =
+          await _firestoreappointment.add(appointmentsData);
 
-      String appointmentId = docRef.id;
+      String appointmentId = docRef1.id;
 
-      await docRef.update({
+      await docRef1.update({
         FirebaseStrings.appointmentId: appointmentId,
       });
 
-      await _firestoresub.doc(appointmentId).set({
-        FirebaseStrings.productName: product.productName,
-        FirebaseStrings.categoryName: product.categoryName,
-        FirebaseStrings.ateleName: product.ateleName,
-        FirebaseStrings.phoneNumber: product.phoneNumber,
-        FirebaseStrings.address: product.address,
-        FirebaseStrings.appointmentDate: date,
-        FirebaseStrings.appointmentTime: time,
-        FirebaseStrings.depositeAmount: product.depositeAmount,
-        FirebaseStrings.price: product.price,
-        FirebaseStrings.rest: product.price - product.depositeAmount,
-        FirebaseStrings.productsImages: product.productImages,
-        FirebaseStrings.isForRent: product.isForRent,
-        FirebaseStrings.productId: product.productId,
-        FirebaseStrings.userId: FirebaseAuth.instance.currentUser!.uid,
+      await _firestoreSubUser.doc(appointmentId).set(appointmentsData);
+      await _firestoreSubUser.doc(appointmentId).update({
         FirebaseStrings.appointmentId: appointmentId,
-        FirebaseStrings.status: FirebaseStrings.statusPending,
       });
+      final productData = await getProductById(product.productId);
+      await _firestoreSubSeller
+          .doc(productData![FirebaseStrings.sellerId])
+          .collection(FirebaseStrings.appointments)
+          .doc(appointmentId)
+          .set(appointmentsData);
+          
+      await _firestoreSubSeller
+          .doc(productData![FirebaseStrings.sellerId])
+          .collection(FirebaseStrings.appointments)
+          .doc(appointmentId)
+          .update({
+        FirebaseStrings.appointmentId: appointmentId,
+      });
+
       await updateStock(product);
     } catch (e) {
       print('Error adding appointment: $e');
@@ -81,6 +88,17 @@ class ReserveCubit extends Cubit<ReserveState> {
       });
     } catch (e) {
       print('Error updating stock: $e');
+    }
+  }
+
+  Future<Map<String, dynamic>?> getProductById(String productId) async {
+    try {
+      DocumentSnapshot productSnapshot =
+          await _firestoreProduct.doc(productId).get();
+      return productSnapshot.data() as Map<String, dynamic>;
+    } catch (e) {
+      print('Error getting product by ID: $e');
+      return null;
     }
   }
 }
